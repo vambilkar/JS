e18bbd29d3908eaf37056f46d53e15aa5c82a4d4
vambilkar <ambilkarv814@gmail.com>
2024-10-03 08:30:35 +0000

    variables completed

---
+++ Check/Variables.js
@@ -0,0 +1,13 @@
+const accountId = 100
+let email = "ambilkarvaibhav@gmail.com"
+var accountPassword = "1234"
+accountCity="Jaipur"
+
+console.table([accountId,email,accountPassword,accountCity])
+
+/*
+Prefer not to use var beacause of issue in block scope and functional scope
+
+*/
+const v=0;
+console.log(v)
