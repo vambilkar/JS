1a30676f561359760e47c32e67172718400a510a
vambilkar <ambilkarv814@gmail.com>
2024-10-03 09:46:47 +0000

    Conversion details

---
+++ Check/Conversion.js
@@ -0,0 +1,51 @@
+let score = "33av22"
+
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// //"33" => 33
+// "33abc" => NaN
+// true =>1 
+
+let isLoggedIn =0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+// ***********Operation****************
+
+let value = 3 
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("2"+3)
+
+console.log("2" + 2 +2)
+console.log(1 + 2 +"3")
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3 
+num1= num2 = num3 = 2+2;
+
+let gameConter = 100
+gameConter++;
+console.log(gameConter);
+++gameCouter;
+
+
+
