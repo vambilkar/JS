5462a643040397c5ae5a3eecaa93508b1adb2ecd
vambilkar <ambilkarv814@gmail.com>
2024-10-06 07:32:09 +0000

    Date and Time

--- Check/Number_Maths.js
+++ Check/Number_Maths.js
@@ -1,2 +1,29 @@
-const a = new Number(100)
-console.log(a);
+// const a = new Number(100)
+// console.log(a);
+// console.log(a.length);
+// console.log(a.toFixed(2))
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(2))
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN')); //IndianValue
+
+
+//+++++++++++++++++++++Maths+++++++++++++++++++++++++++
+
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+
+
+
+
+
+
+
+
+
